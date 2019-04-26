15bb5739881b11aef73a4c019dff5a9dc4c799e1
hbwjj <42823490+hbwjj@users.noreply.github.com>
2019-04-26 08:27:17 +0000

    Delete order_clean.sh

--- gkznkr/order_clean.sh
+++
@@ -1,3 +0,0 @@
-hadoop fs -rm /user/hive/warehouse/gkznkr.db/prd_order_day/ptday=20180812/*
-hadoop fs -rmdir /user/hive/warehouse/gkznkr.db/prd_order_day/ptday=20180812
-hive -e "alter table gkznkr.prd_order_day drop if exists partition (ptday=20180812);"
